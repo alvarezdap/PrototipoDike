16cc9429b4b5f06522062583db40910d120454b1
alvarezdap <alvarezdap@pm.me>
2023-05-12 13:15:25 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,13 @@
 import streamlit as st
 
-accion = st.selectbox("Accion",('RESCISIÓN DE CONTRATO','AMPARO POSESORIO'))
+accion = st.numeralselectbox("Accion",('RESCISIÓN DE CONTRATO','AMPARO POSESORIO'))
 
 if accion == 'RESCISIÓN DE CONTRATO':
-    st.multiselect('Codigo Civil',['1457', '1708, 1828 y 1829', '1764', '1777', '1797', '1797 y 1798', '1798', '1800', '1806', 
-                                   '1807', '1828', '1829', '1831', '1833', '1836', '1855', '2392', '2393', '580', '599', '603', 
-                                   '686', '691', '702', 'el codigo civil en el  1828'])
+    st.multiselect('Codigo Civil',['580', '599','603', '686', '691', '702','1457','1708','1764', '1777', '1797','1798','1800', '1806', '1807', '1828','1829','1831', '1833', '1836', '1855', '2392', '2393'])
+    st.multiselect('Código Orgánico General de Procesos',['76','89','107','148','168','233','234','256','262','262 numeral 3', '284', '289', '291', '294', '295', '297'])
+    st.multiselect('Constitución de la República del Ecuador',['1','75','76','76 numeral 7','76 numeral 7 literal h)','76 numeral 7 literal l)','82','167', '169', '172', '174 inciso segundo'])
+    st.multiselect('Código Orgánico de la Función Judicial',['1','18','28','29','129','150','152','240'])
 
-#codigo civil,codigo_organico_general_de_procesos,constitucion_de_la_republica,codigo_organico_de_la_funcion_judicial
-#","['. 3 del  262 del', '107 del', '148 del', '168 del', '233 del', '233 y 234 del', '256 del', '262 del', '284 del', '289  del', '291 del', '294 del', '295 del', '297 del', '76 del', '89 del']","['. 7 del  76', '1, 76, 82 y 172', '167', '169', '169 de la ca magna', '169 y 172', '169 y 76', '174 inciso segundo', '75 de la ca magna', '76', '76 . 7 literal 1)', '76, . 7, literal l,', '82', 'literal h) del . 7 del  76', 'literal l) del . 7 del  76']","['1', '129', '150 y 152', '18 y 240', '240', '28 y 29']"
+
+
+
