d1c4fad6ed0c7de9dbd1b793759cb8452eedc7c7
alvarezdap <alvarezdap@pm.me>
2023-05-12 12:43:25 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,5 +7,5 @@ if accion == 'RESCISIÓN DE CONTRATO':
                                    '1807', '1828', '1829', '1831', '1833', '1836', '1855', '2392', '2393', '580', '599', '603', 
                                    '686', '691', '702', 'el codigo civil en el  1828'])
 
-codigo civil,codigo_organico_general_de_procesos,constitucion_de_la_republica,codigo_organico_de_la_funcion_judicial
-","['. 3 del  262 del', '107 del', '148 del', '168 del', '233 del', '233 y 234 del', '256 del', '262 del', '284 del', '289  del', '291 del', '294 del', '295 del', '297 del', '76 del', '89 del']","['. 7 del  76', '1, 76, 82 y 172', '167', '169', '169 de la ca magna', '169 y 172', '169 y 76', '174 inciso segundo', '75 de la ca magna', '76', '76 . 7 literal 1)', '76, . 7, literal l,', '82', 'literal h) del . 7 del  76', 'literal l) del . 7 del  76']","['1', '129', '150 y 152', '18 y 240', '240', '28 y 29']"
+#codigo civil,codigo_organico_general_de_procesos,constitucion_de_la_republica,codigo_organico_de_la_funcion_judicial
+#","['. 3 del  262 del', '107 del', '148 del', '168 del', '233 del', '233 y 234 del', '256 del', '262 del', '284 del', '289  del', '291 del', '294 del', '295 del', '297 del', '76 del', '89 del']","['. 7 del  76', '1, 76, 82 y 172', '167', '169', '169 de la ca magna', '169 y 172', '169 y 76', '174 inciso segundo', '75 de la ca magna', '76', '76 . 7 literal 1)', '76, . 7, literal l,', '82', 'literal h) del . 7 del  76', 'literal l) del . 7 del  76']","['1', '129', '150 y 152', '18 y 240', '240', '28 y 29']"
