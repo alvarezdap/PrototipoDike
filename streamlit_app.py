dd8c2087c00aeebdb8c1d07a0450029f55816127
alvarezdap <alvarezdap@pm.me>
2023-05-12 14:08:49 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,8 +15,8 @@ if accion == 'RESCISIÓN DE CONTRATO':
         st.markdown("Las fundaciones de beneficencia que hayan de administrarse por una agrupación de individuos, se regirán por los estatutos que el fundador les hubiere dictado; y si el fundador no hubiere manifestado su voluntad a este respecto, o sólo la hubiere manifestado incompletamente, se suplirá esta falta por el Presidente de la República.")
         st.header("599")
         st.markdown("El dominio, que se llama también propiedad, es el derecho real en una cosa corporal, para gozar y disponer de ella, conforme a las disposiciones de las leyes y respetando el derecho ajeno, sea individual o social. La propiedad separada del goce de la cosa, se llama mera o nuda propiedad.")
-        codigo_civil=st.multiselect('Código Civil',['580', '599','603', '686', '691', '702','1457','1708','1764', '1777', '1797','1798','1800', '1806', '1807', '1828','1829','1831', '1833', '1836', '1855', '2392', '2393'])
-        
+        codigo_civil=st.multiselect('Selecciona los artículos a usar',['580', '599','603', '686', '691', '702','1457','1708','1764', '1777', '1797','1798','1800', '1806', '1807', '1828','1829','1831', '1833', '1836', '1855', '2392', '2393'])
+        st.write('Seleccionaste:', codigo_civil)
 
     with tab2:
         st.header("Código Orgánico General de Procesos")
